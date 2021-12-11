e80f3f9c3f1271aea781027b80e2425f696d60f9
Ivan Tkachuk <94393106+ivtkac@users.noreply.github.com>
2021-12-11 20:16:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,8 +33,7 @@ namespace MongoDBSimple
           Console.WriteLine("Enter name of Collection to Show: <Customers> | <TypesLoans> | <ProvidedLoans>");
           Console.Write("> ");
           string? collection = Console.ReadLine();
-          while (collection != "Customers" || collection != "TypesLoans"
-            || collection != "ProvidedLoans")
+          while (collection != "Customers" || collection != "TypesLoans" || collection != "ProvidedLoans")
           {
             if (collection == "Customers" || collection == "TypesLoans" || collection == "ProvidedLoans") break;
             Console.Write("> "); collection = Console.ReadLine();
